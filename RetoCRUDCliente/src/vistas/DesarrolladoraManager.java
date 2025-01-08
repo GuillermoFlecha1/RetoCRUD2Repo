e5bd6190fb915ce95dd602500b527f4b9f882f70
Guille <willros@gmail.com>
2025-01-08 08:46:49 +0100

    Servidor Guille con las Queries en mi RestFul.

---
+++ RetoCRUDCliente/src/vistas/DesarrolladoraManager.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistas;
+
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.WebTarget;
+
+/**
+ *
+ * @author 2dam
+ */
+interface DesarrolladoraManager {
+
+    //Hay que cambiar todo a la entidad de cada uno. Para que no sea genérico.
+    public String countREST() throws ClientErrorException;
+
+    public void edit_XML(Object requestEntity, String id) throws ClientErrorException;
+
+    public void edit_JSON(Object requestEntity, String id) throws ClientErrorException;
+    
+    public <T> T find_XML(Class<T> responseType, String id) throws ClientErrorException;
+
+    public <T> T find_JSON(Class<T> responseType, String id) throws ClientErrorException;
+
+    public <T> T findRange_XML(Class<T> responseType, String from, String to) throws ClientErrorException ;
+
+    public <T> T findRange_JSON(Class<T> responseType, String from, String to) throws ClientErrorException;
+
+    public void create_XML(Object requestEntity) throws ClientErrorException;
+
+    public void create_JSON(Object requestEntity) throws ClientErrorException;
+    
+    public <T> T findAll_XML(Class<T> responseType) throws ClientErrorException;
+
+    public <T> T findAll_JSON(Class<T> responseType) throws ClientErrorException;
+
+    public void remove(String id) throws ClientErrorException;
+}
